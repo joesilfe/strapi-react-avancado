33729775b6ffaa973ac3ba5bd3d07482e1d9de5a
Joel Silva <joel.silva.ferr@gmail.com>
2021-01-16 22:23:23 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c7ec603d31e02089ce15d9d68d396345'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'dadf1cd9-0937-4104-8757-a8aa233172fb'
+};
